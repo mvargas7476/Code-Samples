2e5db3cf19f0cde1c8bbb9848a3d2a43e243c323
mvargas7476 <vargas.matias21@gmail.com>
2020-01-20 19:20:49 -0600

    iOS Applications

---
+++ iOS-Applications/(1)I-am-Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Logic and Code created by Matias Vargas
+//  Material and visual components made London App Brewery through Udemy
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
